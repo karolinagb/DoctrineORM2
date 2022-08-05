6ca50ca7edd34309a0c4c24b0ed2d22f38537ba9
x <karolina_bp@hotmail.com>
2022-08-05 14:29:54 -0300

    Revisando o código, removendo e adicionando novos registros

--- bin/insert-student.php
+++ bin/insert-student.php
@@ -7,8 +7,12 @@ use Alura\Doctrine\Helper\EntityManagerCreator;
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $entityManager = EntityManagerCreator::createEntityManager();
+$student = new Student($argv[1]);
+//argc = faz um count no argv retornando quantos parametros foram passados (incluído o próprio script php)
+for ($i = 2; $i < $argc; $i++) {
+    $student->addPhone(new Phone($argv[$i]));
+}
 
-$student = new Student('Aluno com telefones');
 $student->addPhone(new Phone('(21) 99999 - 9999'));
 $student->addPhone(new Phone('(21) 2222 - 2222'));
 
