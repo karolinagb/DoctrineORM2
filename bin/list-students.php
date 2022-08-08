2a161bf85ad635971c817b84ddfa2ef354ac2256
x <karolina_bp@hotmail.com>
2022-08-08 15:01:22 -0300

    Conhecendo DQL

--- bin/list-students.php
+++ bin/list-students.php
@@ -8,10 +8,16 @@ use Alura\Doctrine\Helper\EntityManagerCreator;
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $entityManager = EntityManagerCreator::createEntityManager();
-$studentRepository = $entityManager->getRepository(Student::class);
+// $studentRepository = $entityManager->getRepository(Student::class);
+
+//DQL = Doctrine Query Language - para criar querys sem usar SQL, é bem parecido com SQL, mas ao invés de tabelas usamos entidades
+$dql = "SELECT student FROM Alura\\Doctrine\\Entity\\Student student"; //buscando todos os alunos do bd
+//createQuery() = retorna uma query
+//getResult() = pega o resultado da query - transforma os dados em entidade
+$studentList = $entityManager->createQuery($dql)->getResult(); 
 
 /** @var Student[] $studentList */
-$studentList = $studentRepository->findAll();
+// $studentList = $studentRepository->findAll();
 
 foreach ($studentList as $student) {
     echo "ID: $student->id\nNome: $student->name";
@@ -37,4 +43,7 @@ foreach ($studentList as $student) {
     echo PHP_EOL . PHP_EOL;
 }
 
-echo $studentRepository->count([]) . PHP_EOL;
+// echo $studentRepository->count([]) . PHP_EOL;
+echo count($studentList) . PHP_EOL;
+
+
--- src/Helper/EntityManagerCreator.php
+++ src/Helper/EntityManagerCreator.php
@@ -2,8 +2,11 @@
 
 namespace Alura\Doctrine\Helper;
 
+use Doctrine\DBAL\Logging\Middleware;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\ORMSetup;
+use Symfony\Component\Console\Logger\ConsoleLogger;
+use Symfony\Component\Console\Output\ConsoleOutput;
 
 class EntityManagerCreator
 {
@@ -13,6 +16,20 @@ class EntityManagerCreator
             [__DIR__ . "/.."],
             true
         );
+
+        //Middleware é um intermediário, quando estamos fazendo um processo, ele intercepta e faz outra coisa no meio do caminho
+            //setMiddlewares aceita um array de objetos que implemente uma interface de middleware
+            //Vamos adicionar um midleware para fazer um log da query que vai ser executada
+            //O doctrine já fornece a classe Middleware do namespace de Logging
+        $config->setMiddlewares([
+            //Recebe como parãmetro um objeto que implemente uma interface de Logger
+                //O doctrine já fornece um log que é o ConsoleLogger
+            new Middleware(
+                //A saída no console pode ser formata de vários jeitos então preciso passar como parâmetro um output
+                //o doctrine fornece o ConsoleOutput
+                //Para mostrar o sql temos que informar que queremos o maior número de informações possível com VERBOSITY_DEBUG
+                new ConsoleLogger(new ConsoleOutput(ConsoleOutput::VERBOSITY_DEBUG)))
+        ]);
 
         $conn = [
             'driver' => 'pdo_sqlite',
